3d560ec46b59dfa9bb84f08d4f258a36a1f3eddf
Sandeeb Adhikari <sa2734@njit.edu>
2024-11-30 07:27:51 -0500

    sidebar css

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,14 +3,22 @@ import Image from "next/image";
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 
+import { routines } from "@/constants";
+
 interface SidebarProps {
   side: "left" | "right";
 }
 
-const weekday = new Date().toLocaleDateString("en-US", { weekday: "long" });
-
 const Sidebar: React.FC<SidebarProps> = ({ side }) => {
   const [date, setDate] = useState(new Date());
+  const weekday = new Date()
+    .toLocaleDateString("en-US", { weekday: "long" })
+    .toUpperCase() as keyof typeof routines;
+  const currentTime = new Date().toTimeString().slice(0, 5);
+
+  const upcomingRoutines =
+    routines[weekday]?.filter((routine) => routine.time > currentTime) || [];
+
   return (
     <div className="w-[429px] flex flex-col items-center mt-10 mx-5 border">
       {side === "left" && (
@@ -29,16 +37,37 @@ const Sidebar: React.FC<SidebarProps> = ({ side }) => {
       )}
       {side === "right" && (
         <div className="flex flex-col p-2 items-center">
-          <div className="p-2 rounded-md shadow-md align-middle">
+          <div className="mt-10 rounded-md shadow-md align-middle">
             <Calendar
               onChange={(value) => value && setDate(value as Date)}
               value={date}
               className="p-2 rounded-md flex flex-col items-center justify-center"
             />
           </div>
-          <p className=" mt-4 text-sm text-gray-600">
-            Selected Date: {date.toDateString()}
-          </p>
+
+          <h2 className="text-xl mt-4 font-bold text-center">
+            {weekday.toUpperCase()}
+            <span className="text-gray-500"> ROUTINE</span>
+          </h2>
+          <div className="mt-4 w-full h-[118px] bg-[#171717] overflow-auto no-scrollbar  rounded-md">
+            {upcomingRoutines.length > 0 ? (
+              <ul className="p-1 text-sm text-gray-300">
+                {upcomingRoutines.map((routine, index) => (
+                  <li
+                    key={index}
+                    className="flex justify-between p-1 hover:bg-gray-500 hover:rounded-md hover:scale-95 transition-transform ease-in"
+                  >
+                    <span>{routine.time}</span>
+                    <span>{routine.activity}</span>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p className="text-sm text-gray-500 text-center mt-4 no-underline">
+                No more routines for today.
+              </p>
+            )}
+          </div>
         </div>
       )}
     </div>
