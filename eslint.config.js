e2c9d15e4869d9e8de9abd4478f3d232668f4849
Sandeeb Adhikari <sa2734@njit.edu>
2024-12-04 09:38:29 -0500

    latest version install eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,40 @@
-import next from "eslint-config-next";
+const next = require("eslint-config-next");
+const typescriptPlugin = require("@typescript-eslint/eslint-plugin");
+const typescriptParser = require("@typescript-eslint/parser");
+const tailwindcssPlugin = require("eslint-plugin-tailwindcss");
 
-export default [
+module.exports = [
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
-    ignores: ["node_modules", "dist"],
-    rules: {},
+    ignores: ["node_modules", "dist", ".next"],
     languageOptions: {
+      parser: typescriptParser,
       parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
+        project: "./tsconfig.json",
       },
     },
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
+    plugins: {
+      "@typescript-eslint": typescriptPlugin,
+      tailwindcss: tailwindcssPlugin,
     },
     extends: [next()],
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "react/react-in-jsx-scope": "off",
+      "no-console": "warn",
+      "tailwindcss/no-custom-classname": "off",
+      "tailwindcss/classnames-order": "warn",
+      "tailwindcss/no-contradicting-classname": "error",
+    },
+    settings: {
+      tailwindcss: {
+        config: "./tailwind.config.ts", // Path to your Tailwind config
+      },
+    },
   },
 ];
